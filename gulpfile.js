ed71ee7ca85b72100bc2856ae237d9493ffba118
Mikael Brassman <mishakun@gmail.com>
2014-09-02 09:46:50 +0200

    Added watch task and page set to landscape

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence'),
     tap = require('gulp-tap'),
     sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
     asCard = require('./lib/as-card'),
     toHtml = require('./lib/to-html');
 
 var loadedCards = [];
 
 gulp.task('load-cards', function() {
+    loadedCards = [];
     return gulp.src('./src/cards/*.md')
         .pipe(tap(function(file) {
             loadedCards.push(asCard(file));
@@ -15,18 +17,24 @@ gulp.task('load-cards', function() {
 });
 
 gulp.task('to-html', function() {
-    toHtml('./src/jade/template.jade', loadedCards)
+    return toHtml('./src/jade/template.jade', loadedCards)
         .pipe(gulp.dest('build/'));
 });
 
 gulp.task('sass', function() {
     return gulp.src('./src/sass/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('build/'));
+        .pipe(watch(function(files) {
+            return files
+                .pipe(sass())
+                .pipe(gulp.dest('build/'));
+        }));
 });
 
 gulp.task('build', function() {
-    runSequence('load-cards', 'to-html');
+    gulp.src('./src/cards/*.md')
+        .pipe(watch(function(files) {
+            return runSequence('load-cards', 'to-html');
+        }));
 });
 
 gulp.task('default', ['sass', 'build'], function() {
